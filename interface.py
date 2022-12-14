8b49198fe03dce17435b58de66f9aa19b041b50f
Gustominox <agstcmps.111@gmail.com>
2022-12-14 10:35:10 +0000

    Debug and refactor code from @LexerPlay

--- interface.py
+++ interface.py
@@ -1,58 +1,53 @@
 import pygame
 import pygame.locals
 from mapa import Mapa
-
+from player import Player
+from vector import Vector
 #   Initialize th pygame
 pygame.init()
 
 #size of matrix * pixels
-lines = 7
-rows = 10
-size = width, height = 64*rows, 64*lines
+size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
+SCALE = 128
 
 # Title and icon
 pygame.display.set_caption("VectorRace")
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load('imgs/icon.png')
 pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('imgs/racecar.png')
+playerImg = pygame.transform.rotate(playerImg, 270)
 
 
 
 
 # Desenha o Mapa
-def drawMap(screen):
-    mapa = [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,1,1,0,0,1,1,0],
-        [0,1,1,1,1,1,1,1,1,3],
-        [0,2,1,1,0,0,0,1,1,3],
-        [0,1,1,1,1,1,1,1,1,3],
-        [0,0,0,0,1,1,1,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0]
-        ]
+def drawMap(screen,mapa):
+    content = mapa.content
     colors = [pygame.Color("black"), pygame.Color("grey"), pygame.Color("green"), pygame.Color("red")]
-    for r in range(lines):
-        for c in range(rows):
-            pygame.draw.rect(screen, colors[mapa[r][c]], pygame.Rect(c*64, r*64, 64, 64))
+    for line in range(mapa.lines):
+        for row in range(mapa.rows):
+            pygame.draw.rect(screen, colors[content[line][row]], pygame.Rect(row*SCALE, line*SCALE, SCALE, SCALE))
             
-
-
-
-# Player
-playerImg = pygame.image.load('racecar.png')
-playerImg = pygame.transform.rotate(playerImg, 270)
-playerX = 1*64
-playerY = 3*64
-
-def player():
+def player(player):
+    playerX = player.estado.x * SCALE
+    playerY = player.estado.y * SCALE
     screen.blit(playerImg,(playerX,playerY))
 
-
+def movePlayer(Player):
+    
     
 
 
 # Game Loop
 running = True
+
+mapa = Mapa("tracks/track.txt")
+jogador = Player(Vector(1,3))
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -61,8 +56,8 @@ while running:
     # Background colour
     screen.fill((50, 50, 50))
 
-    drawMap(screen)
-    player()
+    drawMap(screen,mapa)
+    player(jogador)
     pygame.display.update()
 
 
