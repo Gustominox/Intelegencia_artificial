28717c9fbb2a834dee530db6dbf68e8cc3f8145a
Gustominox <agstcmps.111@gmail.com>
2022-12-24 11:50:23 +0000

    small refactoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Importar classes nodo e grafo
+# Importar classes nodo e grafo
 from grafo import Graph
 from nodo import Node
 from mapa import Mapa
@@ -11,7 +11,6 @@ from interface import *
 
 
 def main():
-
 
     running = True
 
@@ -53,10 +52,8 @@ def main():
 
             start = createButton("Start", 0, COLOR_BLACK)
             maps = createButton("Choose Map", 1, COLOR_BLACK)
-            algoritmos = createButton(
-                "Choose Algorithm(s)", 2, COLOR_BLACK)
-            players = createButton(
-                "Choose Number of players", 3, COLOR_BLACK)
+            algoritmos = createButton("Choose Algorithm(s)", 2, COLOR_BLACK)
+            players = createButton("Choose Number of players", 3, COLOR_BLACK)
             calcPath = createButton("Calculate best path", 4, COLOR_BLACK)
             quit_game = createButton("Quit", 6, COLOR_BLACK)
 
@@ -68,7 +65,8 @@ def main():
 
                     alg_selected = jogador.alg_selected
 
-                    jog = resolver.getJog(alg_selected,jogador, mapa.finish, grafo)
+                    jog = resolver.getJog(
+                        alg_selected, jogador, mapa.finish, grafo)
 
                     possivelPosicao = jogador.estado + jogador.velocidade + jog
 
@@ -84,7 +82,7 @@ def main():
                         jogador.estado = novaPosicao
                         jogador.velocidade = Vector(0, 0)
                         break
-                    
+
             drawMap(screen, mapa, jogo.getPlayers())
             drawText("ESC to return", FONT, COLOR_WHITE, 50, 50)
 
@@ -98,14 +96,12 @@ def main():
             if game_menu == "maps":
                 # event mouse
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    option = buttonIndex([map1, map2, map3],mousePos)
-                    
-                    print(f"OPTION {map_selected}")
+                    option = buttonIndex([map1, map2, map3], mousePos)
 
                     if option == 0:
                         mapa = Mapa("tracks/track.txt")
                         map_selected = 0
-                        
+
                     elif option == 1:
                         mapa = Mapa("tracks/trackCircle.txt")
                         map_selected = 1
@@ -117,24 +113,19 @@ def main():
                         game_menu = 'main_menu'
                     else:
                         pass
-                    
+
                     map1, map2, map3, leave = drawMapsMenu(
-                            map_selected)
-                    
+                        map_selected)
+
                     grafo = Graph()
                     grafo.createGraphCartesian(mapa)
-
-                # if event.type == pygame.KEYDOWN:
-                #     if event.key == pygame.K_ESCAPE:
-                #         game_menu = 'main_menu'
 
             elif game_menu == "algoritmos":
                 # event mouse
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     option = buttonIndex([a_estrela, greedybf, greedy, bfs, dfs, a_estrela2,
-                                        greedybf2, greedy2, bfs2, dfs2, leave_alg], mousePos)
-                
-                
+                                          greedybf2, greedy2, bfs2, dfs2, leave_alg], mousePos)
+
                     if option > 0 and option < 5:
 
                         jogo.players[0].alg_selected = option
@@ -149,57 +140,57 @@ def main():
                         pass
                 a_estrela, greedybf, greedy, bfs, dfs, a_estrela2, greedybf2, greedy2, bfs2, dfs2, leave_alg = drawAlgoritmosMenu(
                     alg_selected, jogo.players)
-                
+
             elif game_menu == "calcPath":
                 # event mouse
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    option = buttonIndex([a_estrela, greedybf, greedy, bfs, dfs, leave], mousePos)
-                    
+                    option = buttonIndex(
+                        [a_estrela, greedybf, greedy, bfs, dfs, leave], mousePos)
+
                     inicio = mapa.start
-                    fim = mapa.finish 
-                
-                    if option == 0 :
-                        
-                                        
+                    fim = mapa.finish
+
+                    if option == 0:
+
                         pass
-                    elif option == 1 :
+                    elif option == 1:
                         pass
-                    elif option == 2 :
-                    
-                        (path, custoT) = resolver.greedy_search(inicio, fim,grafo, path=[])
-                        p = [(vector.x,vector.y)for vector in path]
-                    
-                    
-                        mapa.show()   
-                        showPath(p)
-                        plt.show()
-                        
-                    elif option == 3 :
-                        
-                        (path, custo) = resolver.bfs(inicio, fim,grafo)
-                        p = [(vector.x,vector.y)for vector in path]
-                        
-                        mapa.show()   
-                        showPath(p)
-                        plt.show()
-                        
-                    elif option == 4 :
-                            
-                        (path, custoT) = resolver.dfs( inicio, fim,grafo, path=[], visited=set())
-                        p = [(vector.x,vector.y)for vector in path]
-                    
-                        mapa.show()   
+                    elif option == 2:
+
+                        (path, custoT) = resolver.greedy_search(
+                            inicio, fim, grafo, path=[])
+                        p = [(vector.x, vector.y)for vector in path]
+
+                        mapa.show()
                         showPath(p)
                         plt.show()
 
+                    elif option == 3:
+
+                        (path, custo) = resolver.bfs(inicio, fim, grafo)
+                        p = [(vector.x, vector.y)for vector in path]
+
+                        mapa.show()
+                        showPath(p)
+                        plt.show()
+
+                    elif option == 4:
+
+                        (path, custoT) = resolver.dfs(
+                            inicio, fim, grafo, path=[], visited=set())
+                        p = [(vector.x, vector.y)for vector in path]
+
+                        mapa.show()
+                        showPath(p)
+                        plt.show()
 
                     elif leave.collidepoint(mousePos):
                         game_menu = 'main_menu'
                     else:
                         pass
-                    
+
                 a_estrela, greedybf, greedy, bfs, dfs, leave = drawPathMenu()
-                    
+
             elif game_menu == "players":
 
                 # event mouse
@@ -262,11 +253,11 @@ def main():
 
                         one, two, leave = drawJogMenu(
                             players_selected)
-                        
+
                     if calcPath.collidepoint(mousePos):
                         game_menu = "calcPath"
 
-                        a_estrela_path, greedybf_path, greedy_path, bfs_path, dfs_path,leave = drawPathMenu()
+                        a_estrela_path, greedybf_path, greedy_path, bfs_path, dfs_path, leave = drawPathMenu()
 
                     if quit_game.collidepoint(mousePos):
                         running = False
@@ -275,6 +266,7 @@ def main():
                 running = False
 
         pygame.display.update()
-            
+
+
 if __name__ == "__main__":
     main()
