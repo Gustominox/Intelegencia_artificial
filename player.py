b065d670e02f2242dab1c11ada6543833978caf7
Gustominox <agstcmps.111@gmail.com>
2022-12-24 03:35:04 +0000

    add alg selected

--- player.py
+++ player.py
@@ -7,7 +7,8 @@ class Player:
     
     def __init__(self,estadoInicial):
         self.estado = estadoInicial
-        self.velocidade = Vector(1,1)        
+        self.velocidade = Vector(0,0)
+        self.alg_selected = 0        
     
     def __str__(self):
         return f"Player: Estado -> {self.estado}\tVelocidade -> {self.velocidade}"
